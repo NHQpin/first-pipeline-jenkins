6dc585e34bad0c6bf98396cb9b7e535e8feded87
pin pin <nhq0810job@gmail.com>
2024-08-28 17:31:19 +0700

    update

--- source/config.js
+++ source/config.js
@@ -2,10 +2,10 @@ module.exports = {
     port: 3000, 
 
     database: {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        port: process.env.MYSQ_TCP_PORT
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        port: process.env.port
     }
 };
